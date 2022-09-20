5d54f5e32b2a3e619656b72b8e6b4f1fb235ee3f
Luiz Gabriel Fernandes Alves <luiz.gabriel.ter@estacio.br>
2022-09-20 08:51:30 -0300

    mappers

--- react/src/domain/mappers/product.mapper.ts
+++ react/src/domain/mappers/product.mapper.ts
@@ -1,4 +1,4 @@
-import { ProductDomain } from "./product";
+import { ProductDomain } from "../product";
 
 export const productMapper = (product: any): ProductDomain => {
   return { image: product.image, name: product.name, price: product.price };
